e564bd6c115fbebae6da8f561cb4acca6c9418d3
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-08-28 14:39:26 +0300

    Loop through links in given file

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
+import csv
 import os
-from dotenv import load_dotenv
+import sys
+
+# from dotenv import load_dotenv
 from urllib.request import urlopen
 
-url = "http://olympus.realpython.org/profiles/aphrodite"
-page = urlopen(url)
+def main():
 
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+    # Check for command-line usage
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python app.py <file to process>")
 
-start = html.find("<title>") + len("<title>")
-end = html.find("</title>")
+    with open(sys.argv[1], "r") as file:
+        for url in file:
+            page = urlopen(url)
 
-title = html[start:end]
-print(title)
+            html_bytes = page.read()
+            html = html_bytes.decode("utf-8")
+
+            start = html.find("<title>") + len("<title>")
+            end = html.find("</title>")
+
+            title = html[start:end]
+            print(title)
+
+
+if __name__ == '__main__':
+    main()
