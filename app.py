9aaa7a75b7f0b44dc373173715052e4251f1154d
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-08-29 09:24:34 +0300

    Write to csv file

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python app.py <file to process>")
 
-    with open(sys.argv[1], "r") as file:
+    csvlist = []
+    input_file = sys.argv[1]
+    with open(input_file, "r") as file:
         for url in file:
             page = urlopen(url)
 
@@ -23,6 +25,26 @@ def main():
 
             title = html[start:end]
             print(title)
+            record = {
+                'original url': url,
+                'title': title
+            }
+
+            csvlist.append(record)
+
+    # TODO parse file name from path and replace extension with csv
+    # output_file = input_file.split('.')[0] + '.csv'
+    output_file = ".data/result.csv"
+    print(output_file)
+    fieldnames = ['original url', 'title']
+    with open(output_file, "w", newline='') as csvfile:
+        # TODO write lines from csvlist using DictWriter
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel')
+        writer.writeheader()
+        for row in csvlist:
+            writer.writerow(row)
+
+        pass
 
 
 if __name__ == '__main__':
