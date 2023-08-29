cef0911898c9ec6e18f2845354a514ec981565ee
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-08-29 12:00:40 +0300

    Gather phones

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import csv
-import os
+# import os
 import sys
+import re
 
 # from dotenv import load_dotenv
 from urllib.request import urlopen
@@ -15,26 +16,52 @@ def main():
     input_file = sys.argv[1]
     with open(input_file, "r") as file:
         for counter, url in enumerate(file, start=1):
-            page = urlopen(url)
-            
+
+            # Get page
+            page = urlopen(url)            
             html_bytes = page.read()
             html = html_bytes.decode("utf-8")
 
+            # Find title
             start = html.find("<title>") + len("<title>")
             end = html.find("</title>")
-
             title = html[start:end]
             print(title)
+
+            # Find phone
+            # start = html.find("\"tel:") + len("\"tel:")
+            # end = html.find("\"")
+            start = 0
+            pattern = re.compile(r"\"tel:\+?\d{6,11}\"")
+            phones = []
+            while start < len(html):
+                found = pattern.search(html, pos=start)
+                if found:
+                    start = found.end()
+                    phone = found.group()[5:-1]
+                    # TODO take in acount that numbers starting with 7 or 8 are the same
+                    if not phone in phones:
+                        phones.append(phone)
+
+                # Stop searching if nothing in the end of page
+                else:
+                    break
+
+            # Write to row
             record = {
                 '№': counter,
                 'original url': url,
-                'title': title
+                'title': title,
+                'phone':phones,
+                'e-mail':''
             }
 
+            # Add row of gathered information to list
             csvlist.append(record)
 
+    # Prepare file name to write into, open file and write row by row, starting with header
     output_file = input_file.rsplit('.',1)[0] + '.csv'
-    fieldnames = ['№', 'original url', 'title']
+    fieldnames = ['№', 'original url', 'title', 'phone', 'e-mail']
     with open(output_file, "w", newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel')
         writer.writeheader()
